81887af46af6e775c42325bbbeaaa30fa1515c85
Rishabh Raj <rajrishabh225@gmail.com>
2024-08-11 13:14:41 +0530

    sidebar Ui updated

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
     port:5173,
     proxy: {
       '/api': {
-        // target: 'https://vidsphere-backend.onrender.com/', // Always remember to Replace your backend server URL http://localhost:3000/
-        target:'http://localhost:3000/',
+        target: 'https://vidsphere-backend.onrender.com/', // Always remember to Replace your backend server URL http://localhost:3000/
+        // target:'http://localhost:3000/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/api'),
       },
